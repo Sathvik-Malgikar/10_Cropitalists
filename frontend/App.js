0f2cf06f0eb5f2cd32d4e1b94182faa746d733a0
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2023-02-25 12:05:03 +0530

    created register page

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+
 
 export default function App() {
+  const Drawer = createDrawerNavigator();
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+         <Drawer.Navigator initialRouteName='Register'>
+
+<Drawer.Screen name='Register' component={Register} />
+
+      </Drawer.Navigator>
+    </NavigationContainer>
+   
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
