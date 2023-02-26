aacc004d23d0a17ec592d078351996b74041d9ed
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2023-02-26 10:10:34 +0530

    helpline

--- frontend/About.js
+++ frontend/About.js
@@ -1,6 +1,6 @@
 
 import { useState  } from "react"
-import { StyleSheet, Text, Image, View,TextInput, ScrollView ,TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, Image, View,TextInput, ScrollView } from 'react-native';
 
 import styles from "./styles"
 
@@ -22,7 +22,7 @@ const About = () => {
       <Text style={styles.titlecontain}>How stock values are calculated</Text>
       <Text>This involves a number of parameters and data collected over a period of time. Our calculation algorithm is inspired by the stock market's concept. Based on current demand and local-supply, collected by both external sources as well as our other clients, a stock price is fixed.
       
-      <Text style={styles.link}>Read more</Text>
+   <Text style={styles.link}>Read More</Text>
       </Text>
       <Text style={styles.titlecontain}>Why us?</Text>
       <Text> In the best interest of both the landowner and investor, recommendations are made. We aim to provide an opportunity for investors to spend their idle money into landowners who desperately need money and in return get a share of revenue.
--- frontend/Helpline.js
+++ frontend/Helpline.js
@@ -1,6 +1,6 @@
 
 import { useState  } from "react"
-import { StyleSheet, Text, Image, View,TextInput, ScrollView ,TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, Image, View,TextInput, ScrollView ,TouchableOpacity,Linking,Alert,Platform} from 'react-native';
 
 import styles from "./styles"
 
@@ -12,8 +12,51 @@ const Helpline = () => {
     <View style={styles.formbox}>
       <Text style={styles.titlecontain}>Helpline</Text>
       <Text >For any issues or help : </Text>
-      <Text style={styles.link}>7019486115</Text>
-      <Text style={styles.link}>Cropitalists@outlook.com</Text>
+
+       <TouchableOpacity onPress={()=>{
+      if(Platform.OS == "android"){
+        phoneNumber = `telprompt:7019486115`;
+      }else{
+
+        phoneNumber = `tel:7019486115`;
+      }
+
+      Linking.canOpenURL(phoneNumber)
+  .then(supported => {
+    if (!supported) {
+      Alert.alert('Phone number is not available');
+    } else {
+      return Linking.openURL(phoneNumber);
+    }
+  })
+  .catch(err => console.log(err));
+
+     }}>
+     <Text style={styles.link}>7019486115</Text>
+     </TouchableOpacity> 
+       <TouchableOpacity onPress={()=>{
+      if(Platform.OS == "android"){
+        phoneNumber = `telprompt:7019486115`;
+      }else{
+
+        phoneNumber = `tel:7019486115`;
+      }
+
+      Linking.canOpenURL('message:')
+    .then(supported => {
+        if (!supported) {
+          console.log('Cant handle url')
+        } else {
+          return Linking.openURL('message:')
+        }
+      })
+      .catch(err => {
+        console.error('An error occurred', err)
+      })
+
+     }}>
+     <Text style={styles.link}>Cropitalists@outlook.com</Text>
+     </TouchableOpacity> 
 
 
     </View>
